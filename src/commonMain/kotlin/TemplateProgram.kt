df308842b7ac1afa6ba5ac9a7180cefb45ba8728
nfletton <nigel@thebluehut.com>
2024-12-10 14:21:26 -0400

    Dynamic sketch selection

--- src/commonMain/kotlin/TemplateProgram.kt
+++ src/commonMain/kotlin/TemplateProgram.kt
@@ -1,30 +1,28 @@
+import kotlinx.browser.document
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.extra.noise.Random
-import org.openrndr.extra.shapes.hobbycurve.hobbyCurve
-import org.openrndr.math.Polar
 
-fun main() = application {
+val p = document.getElementById("openrndr-canvas")!!.getAttribute("data-program")!!
+
+fun p1() = application {
     program {
-        console.log("Kotlin says OPENRNDR program started")
-
         extend {
             drawer.clear(ColorRGBa.PINK)
-
-            drawer.fill = null
-            drawer.circle(drawer.bounds.center, 250.0)
-
-            drawer.fill = ColorRGBa.WHITE
-
-            // Create a List of points centered in the window.
-            // Use polar coordinates. The radius is animated using simplex noise.
-            val points = List(12) {
-                val angle = it * 30.0
-                val radius = 200.0 + Random.simplex(it * 0.1, it * 1.7 + seconds * 0.2) * 100.0
-                drawer.bounds.center + Polar(angle, radius).cartesian
-            }
-            // Construct and draw a closed Hobby curve with the points.
-            drawer.contour(hobbyCurve(points, true))
         }
     }
 }
+
+fun p2() = application {
+    program {
+        extend {
+            drawer.clear(ColorRGBa.YELLOW)
+        }
+    }
+}
+
+val apps = mapOf(
+    "p1" to ::p1,
+    "p2" to ::p2,
+)
+
+fun main() = apps[p]!!.invoke()
